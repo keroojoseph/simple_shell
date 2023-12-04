5bb1cd222c9dcf401c33ac801ba4c683de49e6ca
Mark Nader Kamel <91009903+MarkNaderKamel@users.noreply.github.com>
2023-12-04 13:27:13 +0200

    Update vars.c

--- vars.c
+++ vars.c
@@ -5,7 +5,7 @@
  * @info: the parameter struct
  * @buf: the char buffer
  * @p: address of the current position in buf
- *
+ * This project made by Kero & Mark
  * Return: 1 if chain delimiter, 0 otherwise
  */
 int isChainDelimiter(info_t *info, char *buf, size_t *p)
